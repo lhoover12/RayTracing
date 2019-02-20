8455f01198494911cb7ba035edafcb3456633254
Hoover <lukas.s.hoover@optum.com>
2019-02-19 20:28:14 -0600

    image background

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 #include <vector>
 #include <iostream>
 
-
 int emap_width, emap_height;
 struct Material
 {
@@ -161,7 +160,7 @@ bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphe
     return std::min(spheres_dist, checkerboard_dist) < 1000;
 };
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, size_t depth = 0)
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, const std::vector<Vec3f> &emap, size_t depth = 0)
 {
 
     Vec3f point, N;
@@ -170,16 +169,19 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
 
     if (depth > 4 || !scene_intersect(orig, dir, spheres, point, N, material))
     {
-        return Vec3f(.3, .3, .3); // background color
+        //emap[i + 2000 + ((j + 1500) * (emap_width))] just puts the background image on screen
+
+        return Vec3f(1, 1, 1); // background
     }
 
     Vec3f reflect_dir = reflect(dir, N);
     Vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
     Vec3f reflect_orig = reflect_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3; // offset the original point to avoid occlusion by the object itself
     Vec3f refract_orig = refract_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3;
-    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
-    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
-    
+
+    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, emap, depth + 1);
+    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, emap, depth + 1);
+
     float diffuse_light_intensity = 0,
           specular_light_intensity = 0;
 
@@ -195,22 +197,24 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
 };
 void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights, const std::vector<Vec3f> &emap)
 {
-    const int   width    = 1024;
-    const int   height   = 768;
-    const float fov      = M_PI/3.;
-    std::vector<Vec3f> framebuffer(width*height);
+    const int width = 1024;
+    const int height = 768;
+    const float fov = M_PI / 3.;
+    std::vector<Vec3f> framebuffer(width * height);
 
-    #pragma omp parallel for
-    for (size_t j = 0; j<height; j++) { // actual rendering loop
-        for (size_t i = 0; i<width; i++) {
-            float dir_x =  (i + 0.5) -  width/2.;
-            float dir_y = -(j + 0.5) + height/2.;    // this flips the image at the same time
-            float dir_z = -height/(2.*tan(fov/2.));
-            framebuffer[i+j*width] = cast_ray(Vec3f(0,0,0), Vec3f(dir_x, dir_y, dir_z).normalize(), spheres, lights, emap[j][i]);
+#pragma omp parallel for
+    for (int j = height - 1; j >= 0; j--)
+    { // actual rendering loop
+        for (int i = width - 1; i >= 0; i--)
+        {
+            float dir_x = (i + 0.5) - width / 3.;
+            float dir_y = -(j + 0.5) + height / 3.; // this flips the image at the same time
+            float dir_z = -height / (2. * tan(fov / 2.));
+            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 1), Vec3f(dir_x, dir_y, dir_z).normalize(), spheres, lights, emap);
         }
     }
     std::vector<unsigned char> pixmap(height * width * 3);
-    for (size_t i = 0; i < (height * width -1); ++i) //each pixel
+    for (size_t i = 0; i < (height * width - 1); ++i) //each pixel
     {
         Vec3f &c = framebuffer[i]; //vector frame buffer size of screen
                                    //c = address of framebuffer[i]
@@ -219,13 +223,10 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
             c = c * (1. / max);
         for (size_t j = 0; j < 3; j++)
         {
-            pixmap[i*3+j] = (unsigned char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+            pixmap[i * 3 + j] = (unsigned char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
         }
-
     }
-     stbi_write_jpg("out.jpg", width, height, 3, pixmap.data(), 100);
-
-    
+    stbi_write_jpg("out.jpg", width, height, 3, pixmap.data(), 100);
 }
 
 int main()
@@ -239,11 +240,18 @@ int main()
     }
     std::vector<Vec3f> emap;
     emap = std::vector<Vec3f>(emap_height * emap_width);
+    std::cout << " width " << emap_width << std::endl;
+    std::cout << " height " << emap_height << std::endl;
+    std::cout << " size " << emap.size() << std::endl;
+
+    std::cout << " k " << emap_width - 1 << std::endl;
+    std::cout << " l " << emap_height - 1 << std::endl;
+    std::cout << " pos " << (emap_width - 1) + (emap_height - 1) * emap_width << std::endl;
     for (int l = emap_height - 1; l >= 0; l--)
     {
         for (int k = emap_width - 1; k >= 0; k--)
         {
-            emap[k + l * emap_width] = Vec3f(pixmap[(k + l * emap_width) * 3 + 0], pixmap[(k + l * emap_width) * 3 + 1], (k + l * emap_width) * 3 + 2) * (1 / 255.);
+            emap[k + l * emap_width] = Vec3f(pixmap[(k + l * emap_width) * 3 + 0], pixmap[(k + l * emap_width) * 3 + 1], pixmap[(k + l * emap_width) * 3 + 2]) * (1 / 255.);
         }
     }
     stbi_image_free(pixmap);
@@ -256,7 +264,7 @@ int main()
 
     std::vector<Sphere> spheres;
 
-    Sphere sphere(Vec3f(3, 0, -10), 2, ivory); //create sphere
+    Sphere sphere(Vec3f(3, 0, -10), 2, mirror); //create sphere
 
     Sphere sphere2(Vec3f(0, 0, -15), 1.5, glass); //create sphere
 
@@ -264,7 +272,7 @@ int main()
 
     Sphere sphere4(Vec3f(-3, -1, -10), 1.3, brown); //create sphere
 
-    Sphere sphere5(Vec3f(-0, -3, -12), 0.8, mirror); //create sphere
+    Sphere sphere5(Vec3f(-0, -3, -12), 0.8, ivory); //create sphere
 
     spheres.push_back(sphere);
     spheres.push_back(sphere2);
@@ -273,7 +281,7 @@ int main()
     spheres.push_back(sphere5);
 
     std::vector<Light> lights;
-    
+
     lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
     lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
     lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
